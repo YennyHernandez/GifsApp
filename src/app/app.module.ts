c68aa6f93684bcc3900d9786fa6f58eb872457d7
YennyHernandez <yeni.hernandez@udea.edu.co>
2021-09-09 17:30:38 -0500

    Se toma Apikey, peticion http por medio de httpclient modulo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { GifsModule } from './gifs/gifs.module';
+import {HttpClientModule} from '@angular/common/http'
+import { HttpClient} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,8 +13,10 @@ import { GifsModule } from './gifs/gifs.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     SharedModule,
     GifsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/gifs/services/gisfservice.service.ts
+++ src/app/gifs/services/gisfservice.service.ts
@@ -1,23 +1,28 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient} from '@angular/common/http';
+import { HttpClientModule} from '@angular/common/http';
 @Injectable({
   providedIn: 'root'
 })
 export class GisfserviceService {
-
+  private apikey: string = "Bnjhh7LDzHJ248VlmXiXU90EKKz4WtYJ";
   private _historial:string[] = []; // el _ es para indicar que cambia en tiempo real
   get historial(){
     return [...this._historial];
   }
+
+  constructor(private http: HttpClient){}
+
   buscarGifs(query:string=''){
     query = query.toLocaleLowerCase();
     if(!this._historial.includes(query)){ //si no esta incluido, incluyalo
       this._historial.unshift(query);
       this._historial= this.historial.splice(0, 10); //siempre me guardara los 10 primeros
     }
-
-
-    console.log(this._historial);
+    this.http.get('https://api.giphy.com/v1/gifs/search?api_key=Bnjhh7LDzHJ248VlmXiXU90EKKz4WtYJ&q=empanadas&limit=10')
+    .subscribe((resp:any) =>{
+      console.log(resp.data);
+    });T
   }
 
 }
